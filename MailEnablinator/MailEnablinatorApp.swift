0fb5c53dfb19f1b0982b93ba8b60043f168d434e
pdxplaid <adams@cliffdweller.com>
2026-05-26 14:47:13 -0700

    Initial Commit

---
+++ MailEnablinator/MailEnablinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailEnablinatorApp.swift
+//  MailEnablinator
+//
+//  Created by Paul Adams on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct MailEnablinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
